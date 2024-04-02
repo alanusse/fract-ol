79661625dbaa6752f2af8bb0263ed824d71e02ae
Agustin Lanusse <lanusseagustin@gmail.com>
2024-04-02 19:05:36 +0200

    init

---
+++ includes/fractol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 18:45:12 by aglanuss          #+#    #+#             */
+/*   Updated: 2024/04/02 18:57:27 by aglanuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../minilibx-mac/mlx.h"
+
+# define KEY_ESC 53
+# define KEY_UP 126
+# define KEY_DOWN 125
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_SPACE 49
+
+#endif
