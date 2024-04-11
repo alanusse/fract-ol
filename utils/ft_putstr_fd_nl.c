7c8b16be28f9bc96d7b412a234a76c7648d6d2a2
Agustin Lanusse <lanusseagustin@gmail.com>
2024-04-11 12:48:07 +0200

    ft_putstr_fd_nl

--- utils/ft_putstr_fd_nl.c
+++ utils/ft_putstr_fd_nl.c
@@ -1,22 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putstr_fd_nl.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 11:15:52 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/04/11 12:46:51 by aglanuss         ###   ########.fr       */
+/*   Created: 2024/04/11 12:47:10 by aglanuss          #+#    #+#             */
+/*   Updated: 2024/04/11 12:47:26 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-void	ft_putstr_fd(int fd, char *str)
+void	ft_putstr_fd_nl(int fd, char *str)
 {
-	int	len;
-
-	len = ft_strlen(str);
-	if (len > 0)
-		write(fd, str, len);
+  ft_putstr_fd(fd, str);
+  write(fd, "\n", 1);
 }
